8f044d7ebdb90137bc2b47ceb6228205631efd21
Srinivas R <srinivassrini14592@gmail.com>
2025-10-15 17:22:51 +0000

    camerae should on doewn only

--- src/app/user/dashboard/page.tsx
+++ src/app/user/dashboard/page.tsx
@@ -40,10 +40,6 @@ export default function UserDashboardPage() {
             </Alert>
         </div>
 
-        <div>
-            <CameraFeed />
-        </div>
-
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
             <div className="lg:col-span-2">
                 <Card>
@@ -77,8 +73,10 @@ export default function UserDashboardPage() {
                 </CardContent>
             </Card>
         </div>
+        
+        <div>
+            <CameraFeed />
+        </div>
     </div>
   )
 }
-
-    
