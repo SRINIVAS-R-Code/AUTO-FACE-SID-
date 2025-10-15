7746ac0632d4b5f8b0ebbf59c8fd8baa45d359d8
Srinivas R <srinivassrini14592@gmail.com>
2025-10-15 14:48:43 +0000

    adjust coorectly

--- src/app/admin/monitoring/page.tsx
+++ src/app/admin/monitoring/page.tsx
@@ -25,7 +25,7 @@ export default function MonitoringPage() {
 
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         {employeeData.map(employee => (
-          <CameraFeed key={employee.id} />
+          <CameraFeed key={employee.id} employee={employee} />
         ))}
       </div>
     </div>
