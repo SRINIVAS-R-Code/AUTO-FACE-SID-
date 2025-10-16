0187102fa51731721dd249c0feccf5b901fc11bb
Srinivas R <srinivassrini14592@gmail.com>
2025-10-16 07:55:55 +0000

    no erro right i   going to submit my team my frontend

--- src/app/admin/settings/page.tsx
+++ src/app/admin/settings/page.tsx
@@ -414,8 +414,7 @@ function SettingsPage() {
                         render={({ field }) => (
                         <FormItem className="flex flex-row items-center justify-between rounded-lg border p-4">
                             <div className="space-y-0.5">
-                            <FormLabel>Enable Slack Notifications</FormLabel>
-                            <FormDescription>
+                            <FormLabel>Enable Slack Notifications</FormLabel>                            <FormDescription>
                                 Send alerts directly to a Slack channel.
                             </FormDescription>
                             </div>
